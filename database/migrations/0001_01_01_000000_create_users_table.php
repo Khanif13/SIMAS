7ccf0466bcc0cba0cb0c419191bf6fbca0db72ce
khanif13 <ahmadhanif13125@gmail.com>
2026-06-05 23:27:34 +0800

    Update: Make changes in roles

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'sekretaris', 'member'])->default('member');
+            $table->enum('role', ['superadmin', 'admin', 'member'])->default('member');
             $table->rememberToken();
             $table->timestamps();
         });

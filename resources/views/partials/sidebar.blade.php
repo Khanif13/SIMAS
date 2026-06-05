020b551f8774829f52c896a9a89674710261c0a4
khanif13 <ahmadhanif13125@gmail.com>
2026-06-05 23:34:48 +0800

    Update: Make changes in sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -4,7 +4,8 @@
             <h3 class="text-white fw-bold mb-0">
                 <i class="fa-solid fa-folder-open me-2 text-warning"></i>SIMAS
             </h3>
-            <small class="text-white-50">Sistem Pengarsipan</small>
+            <small class="text-white-50">Sistem Informasi Manajemen</small><br>
+            <small class="text-white-50">Arsip Surat</small>
         </div>
 
         <ul class="nav flex-column mt-4">
@@ -13,24 +14,28 @@
                     <i class="fa-solid fa-chart-pie me-2 w-20px"></i> Dashboard
                 </a>
             </li>
+
+            @if (Auth::check() && in_array(Auth::user()->role, ['superadmin', 'admin']))
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->is('incoming-letters*') ? 'active' : '' }}"
+                        href="{{ url('/incoming-letters') }}">
+                        <i class="fa-solid fa-inbox me-2 w-20px"></i> Surat Masuk
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->is('outgoing-letters*') ? 'active' : '' }}"
+                        href="{{ url('/outgoing-letters') }}">
+                        <i class="fa-solid fa-paper-plane me-2 w-20px"></i> Surat Keluar
+                    </a>
+                </li>
+            @endif
+
             <li class="nav-item">
-                <a class="nav-link {{ request()->is('incoming-letters*') ? 'active' : '' }}"
-                    href="{{ url('/incoming-letters') }}">
-                    <i class="fa-solid fa-inbox me-2 w-20px"></i> Surat Masuk
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link {{ request()->is('outgoing-letters*') ? 'active' : '' }}"
-                    href="{{ url('/outgoing-letters') }}">
-                    <i class="fa-solid fa-paper-plane me-2 w-20px"></i> Surat Keluar
-                </a>
-            </li>
-            {{-- <li class="nav-item">
                 <a class="nav-link {{ request()->is('dispositions*') ? 'active' : '' }}"
                     href="{{ url('/dispositions/show') }}">
-                    <i class="fa-solid fa-share-nodes me-2 w-20px"></i> Disposisi
+                    <i class="fa-solid fa-share-nodes me-2 w-20px"></i> Disposisi Saya
                 </a>
-            </li> --}}
+            </li>
         </ul>
     </div>
 </nav>

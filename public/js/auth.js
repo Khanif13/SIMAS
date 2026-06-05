b568d64648f7bdf4abbc28523451ba2531d3e9d0
khanif13 <ahmadhanif13125@gmail.com>
2026-06-05 23:25:24 +0800

    Update: Added js and css for auth

---
+++ public/js/auth.js
@@ -0,0 +1,6 @@
+// File JS khusus untuk halaman autentikasi
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Halaman Login SIMAS dimuat dengan baik.");
+
+    // Nanti logika custom JS (seperti toggle show/hide password) ditaruh di sini
+});
